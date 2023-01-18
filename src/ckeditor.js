8456b46fd7d1da444f2ebf5b3c3572e569a9f01f
Steven Nguyen <steven@Stevens-MacBook-Pro.local>
2023-01-18 12:39:12 +0700

    Update

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,12 +7,12 @@ import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
@@ -25,12 +25,12 @@ Editor.builtinPlugins = [
 	BlockQuote,
 	Bold,
 	Essentials,
+	GeneralHtmlSupport,
 	Heading,
 	Indent,
 	Italic,
 	Link,
 	List,
-	MediaEmbed,
 	Paragraph,
 	SourceEditing,
 	TextTransformation
